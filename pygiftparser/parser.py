cef7ee43f2438039fd0ba406c2978af8000ed21c
CelestineSauvage <celestine.sauvage@gmail.com>
2017-05-05 16:54:39 +0200

    ajout method toEDX

--- pygiftparser/parser.py
+++ pygiftparser/parser.py
@@ -585,7 +585,7 @@ class Question:
         if not self.valid :
             logging.warning (INVALID_FORMAT_QUESTION ) # placement automatique du contenu de body dans une sous-section cours
             return ''
-        return self.answers.toEDX()
+        self.EDX = self.answers.toEDX()
 
     def myprint(self):
         print ("=========Question=========")
