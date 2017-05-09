1b017f0ce53222cb3bab5b2a0f07fed90581cd50
CelestineSauvage <celestine.sauvage@gmail.com>
2017-05-09 15:24:13 +0200

    probleme toEDX

--- pygiftparser/parser.py
+++ pygiftparser/parser.py
@@ -131,7 +131,7 @@ def checkAnswerEssay(expect, ans):
     else:
         return 0
             """)
-        doc.asis('<span id='+self.question.id+'></span>')
+        doc.asis("<span id="+self.question.id+"></span>")
         with doc.tag("script", type="text/javascript"):
             doc.asis("""
     /* The object here is to replace the single line input with a textarea */
@@ -436,7 +436,7 @@ class MultipleChoicesSet(ChoicesSet):
 
     def scriptEDX(self,doc):
         pass
-        
+
 ################# Single answer ######################
 class Answer:
     """ one answer in a list"""
