fa62f7952c173fc3bea662424b5a3cbe77ba7cca
CelestineSauvage <celestine.sauvage@gmail.com>
2017-05-10 10:44:10 +0200

    script edx

--- pygiftparser/parser.py
+++ pygiftparser/parser.py
@@ -139,14 +139,14 @@ def checkAnswerEssay(expect, ans):
             """)
         doc.asis('<span id="'+str(self.question.id)+'"></span>')
         with doc.tag("script", type="text/javascript"):
-            doc.text(u"""
+            doc.text("""
     /* The object here is to replace the single line input with a textarea */
     (function() {
     var elem = $("#"""+str(self.question.id)+"""")
         .closest("div.problem")
         .find(":text");
     /* There's CSS in the LMS that controls the height, so we have to override here */
-    var textarea = $('&#139textarea style="height:150px" rows="20" cols="70"/&#155');
+    var textarea = $('<textarea style="height:150px" rows="20" cols="70"/>');
     console.log(elem);
     console.log(textarea);
     //This is just a way to do an iterator in JS
