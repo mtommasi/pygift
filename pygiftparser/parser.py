ac62db4aae59e9c7ad68728f7f5816bad2a16803
CelestineSauvage <celestine.sauvage@gmail.com>
2017-05-12 11:45:16 +0200

    Ajout EDX pour numerical answer

--- pygiftparser/parser.py
+++ pygiftparser/parser.py
@@ -455,7 +455,7 @@ class NumericAnswer(Answer):
         if match.group('tolerance'):
             print(match.group('tolerance'))
             self.tolerance = float( match.group('tolerance') )
-            print(self.tolerance)
+            print(str(self.tolerance))
         else:
             self.tolerance = 0
     def toHTMLFB(self):
