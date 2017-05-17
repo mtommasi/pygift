e1d8538bcc989c4be8cdcb6848730cea5b6c9178
CelestineSauvage <celestine.sauvage@gmail.com>
2017-05-17 14:20:27 +0200

    nouvelle branche pour separation

--- pygiftparser/utils.py
+++ pygiftparser/utils.py
@@ -35,3 +35,30 @@ def mdToHtml(text,doc=None):
             return
         else :
             return html_text
+
+def moodleRendering(src):
+    """ See https://docs.moodle.org/23/en/Formatting_text#Moodle_auto-format"""
+    # blank lines are new paragraphs, url are links, html is allowed
+    # quick and dirty conversion (don't closed p tags...)
+    src = transformSpecials(src)
+    src = reURL.sub(r'<a href="\1">\1</a>', src)
+    src = reNewLine.sub(r'<p>',src)
+    return src
+
+def htmlRendering(src):
+    return transformSpecials(src)
+
+def markdownRendering(src):
+    return markdown.markdown(transformSpecials(src), MARKDOWN_EXT)
+
+def markupRendering(src,markup='html'):
+    m = sys.modules[__name__]
+    rendering=markup+'Rendering'
+    if rendering in m.__dict__ :
+        return getattr(m,rendering)(src)
+    else:
+        logging.warning('Rendering error: unknown markup language '+markup)
+        return src
+
+def transformSpecials(src):
+    return reSpecialChar.sub(r'\g<char>',src)
