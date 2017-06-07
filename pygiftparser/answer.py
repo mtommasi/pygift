d27993b5c71d98535f5deb0f56f2e50ccd40c166
CelestineSauvage <celestine.sauvage@gmail.com>
2017-06-07 17:08:04 +0200

    suppr suppr attribut in answer

--- pygiftparser/answer.py
+++ pygiftparser/answer.py
@@ -25,9 +25,6 @@ class AnswerSet:
     def __init__(self,question):
         self.question = question
         self.valid = True
-        self.cc_profile = 'ESSAY' # need in toIMS.py
-        # self.max_att = '1'
-
 
     def myprint(self):
         print (self.__class__)
